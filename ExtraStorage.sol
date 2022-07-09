c505caa56940837171e8a48912ab3ffe74e6e30e
nubshark <108732951+nubshark@users.noreply.github.com>
2022-07-09 20:34:44 +0530

    Update ExtraStorage.sol
    'virtual' is not needed here. It has to be used in SimpleStrorage.sol

--- ExtraStorage.sol
+++ ExtraStorage.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.8;
 import "./SimpleStorage.sol";
 
 contract ExtraStorage is SimpleStorage {
-    function store(uint256 _favoriteNumber) public virtual override {
+    function store(uint256 _favoriteNumber) public override {
         favoriteNumber = _favoriteNumber + 5;
     }
 }
