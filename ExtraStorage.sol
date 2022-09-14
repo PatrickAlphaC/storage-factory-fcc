dc224f0135840111eaec5b6fcb8985c2d62fc956
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-15 00:19:24 +0500

    Corrected favourite spelling in ExtraStorage
    Corrected favourite spelling, from "favorite" to "favourite".

--- ExtraStorage.sol
+++ ExtraStorage.sol
@@ -5,8 +5,8 @@ pragma solidity 0.8.8;
 import "./SimpleStorage.sol";
 
 contract ExtraStorage is SimpleStorage {
-    function store(uint256 _favoriteNumber) public override {
-        favoriteNumber = _favoriteNumber + 5;
+    function store(uint256 _favouriteNumber) public override {
+        favouriteNumber = _favouriteNumber + 5;
     }
 }
 
